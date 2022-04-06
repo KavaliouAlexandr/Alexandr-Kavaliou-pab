31adf05d13f86fe606c1f1b9e390d331b32e3607
Alexandr Kavaliou <80467603+KavaliouAlexandr@users.noreply.github.com>
2022-04-06 10:22:26 +0200

    lab-2

--- Projekt2/index.ts
+++ Projekt2/index.ts
@@ -1,15 +1,8 @@
+
 import express from 'express'
 import {Request, Response} from 'express'
 
 const app = express()
-
-let notes = [{
-
-    id:  1,
-    tittle:'aa'
-}]
-
-let note = notes.find(note => note.id === 10)
 
 app.use(express.json())
 
@@ -17,7 +10,7 @@ app.get('/', function (req: Request, res: Response) {
   res.send('GET Hello World')
 })
 app.post('/', function (req: Request, res: Response) {
-  console.log(req.body) // e.x. req.body.title 
+  console.log(req.body) // e.x. req.body.tinbtle 
   res.sendStatus(200).send('POST Hello World')
 })
 
